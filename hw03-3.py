adb08ed11d2daca057239a42976ccb4e543f9dad
TatianaMolofei <ps_tanusha@mail.ru>
2020-12-28 18:31:04 +0300

    hw03

---
+++ hw03-3.py
@@ -0,0 +1,8 @@
+# Нахождение объема куба и площади боковой поверхности при известной длине ребра
+
+length = 5
+cube_volume = length ** 3
+print(cube_volume)
+
+area_side = length ** 2
+print(area_side)
